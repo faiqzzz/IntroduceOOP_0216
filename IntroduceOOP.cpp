242c29bc1dfec3389a2e9475075584768f818ecd
faiqzzz <ridhofaiqzzz@gmail.com>
2025-04-28 19:26:18 +0700

    add character

--- IntroduceOOP.cpp
+++ IntroduceOOP.cpp
@@ -12,31 +12,4 @@ class planet{
         {
             cout  << "my gravity is" << gravity << endl; 
         }
-            void inputdata() { 
-                cout << "my name is : ";
-                cin >> name;
-                cout << "my size is :";
-                cin >> diameters;
-                cout << "my gravity is : ";
-                cin >> gravity;
-
-
-            }
-            
-            
-        
-
-
-};   
-
-int main()
-
-{
-    planet venus;
-    venus.inputdata();
-    cout << " my name is " << venus.name; 
-    cout << "my size is " << venus.diameters; 
-    
-    venus.displaydata();
-
-}
+    };
