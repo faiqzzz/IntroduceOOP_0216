5e39c9b60addab09cbc4e7510875ea40219747d2
faiqzzz <ridhofaiqzzz@gmail.com>
2025-04-28 19:31:41 +0700

    add access and member

--- LanjutanOOP.cpp
+++ LanjutanOOP.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 class menghitungpersegipanjang
+{
+    public : 
+    float panjang; float lebar; float luas;
+    void inputData()
+    {
+        cout << "input panjang :" ;
+        cin >> panjang;
+        cout << "input lebar :" ;
+        cin >> lebar;
+    }
+
 
     
+
+};
